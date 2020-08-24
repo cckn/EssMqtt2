323e4ad7b29198536adcf6585e7537f57b923345
shd00700 <64176753+shd00700@users.noreply.github.com>
2020-08-24 18:29:16 +0900

    test

--- main.go
+++ main.go
@@ -10,28 +10,6 @@ import (
 	"time"
 )
 
-func main() {
-	var wg sync.WaitGroup
-
-	const (
-		uri      = "tcp://broker.hivemq.com:1883"
-		topic    = "test/topic12/17"
-		fileName = "test07.csv"
-	)
-
-	file, _ := os.Create(fileName)
-	defer file.Close()
-
-	println("@@MQTTPublish Start@@")
-	client := Connect("qwertyuiop", uri)
-	wg.Add(1)
-	go listen(&wg, uri, topic, client, *file)
-	wg.Add(2)
-	go Publish(&wg, client, topic)
-
-	wg.Wait()
-}
-
 func Connect(clientId string, uri string) mqtt.Client {
 	opts := createClientOptions(clientId, uri)
 	client := mqtt.NewClient(opts)
@@ -61,10 +39,12 @@ func listen(wg *sync.WaitGroup, uri, topic string, client mqtt.Client, file os.F
 }
 
 func Publish(wg *sync.WaitGroup, client mqtt.Client, topic string) {
-	//for {
-	//	client.Publish(topic, 0, false, "HanGang")
-	//	time.Sleep(time.Second*2)
-	//}
+
+	for {
+		client.Publish(topic, 2, false, "HanGang")
+		time.Sleep(time.Second * 2)
+
+	}
 }
 
 func writeToCsv(file os.File, msg string) {
@@ -74,4 +54,27 @@ func writeToCsv(file os.File, msg string) {
 	record := []string{time.Now().String(), msg}
 	fmt.Println(record)
 	writer.Write(record)
+}
+
+func main() {
+	var wg sync.WaitGroup
+
+	const (
+		uri      = "tcp://broker.hivemq.com:1883"
+		topic    = "test/topic12/17"
+		fileName = "test07.csv"
+	)
+
+	file, _ := os.Create(fileName)
+	defer file.Close()
+
+	println("@@MQTTPublish Start@@")
+	client := Connect("qwertyuiop", uri)
+
+	wg.Add(2)
+
+	go listen(&wg, uri, topic, client, *file)
+	go Publish(&wg, client, topic)
+
+	wg.Wait()
 }
